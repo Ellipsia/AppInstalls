10cf310dd6a69e5270c50455c04a2e1dc0b21f24
Ellipsia <80127443+Ellipsia@users.noreply.github.com>
2021-03-23 08:39:41 -0700

    Add files via upload

--- Install_Alertus.ps1
+++ Install_Alertus.ps1
@@ -1,73 +1,68 @@
-﻿$applicationlist =@("*Alertus*")
-$results = @()
-$classesreglocation = "HKLM:Software\Classes\Installer\Products"
-$uninstreglocations = @("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
+﻿$ApplicationList =@("*Alertus*")
+$Results = @()
+$ClassesRegLocation = "HKLM:Software\Classes\Installer\Products"
+$UninstRegLocations = @("HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall","HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
 $MSI = "alertus-desktopAlert_DotNet4.5_v5.3.3.0.msi"
 $InstallPath = "`"" + $PSScriptRoot + "\$MSI" + "`""
-$logs = "`"C:\Windows\Temp\Alertus.log`""
-$CurrentFaultyInstall = "6B34A75E1C25A74488D065EE7D98D06B"
+$Logs = "`"C:\Windows\Temp\Alertus.log`""
 
 $MSIArguments = @(
     '/i'
     $InstallPath
     '/qn'    
     '/L*v'
-    $logs
+    $Logs
     'REBOOT=REALLYSUPPRESS'
     )
 
 Try{
-    #Seems that if it failed before it cannot rerun because it detects the registry key for the current version
-    If ($classesreglocation + "\" + $CurrentFaultyInstall){
-        Remove-Item ($classesreglocation + "\" + $CurrentFaultyInstall) -Recurse -Force -ErrorAction SilentlyContinue
-    }
+    $IssueReported = $null
 
-    #Try install, if fail clean up registry of older version 
-    Stop-Process -Name "msi*" -Force -ErrorAction SilentlyContinue
-    $Install = Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow -ErrorAction Stop -PassThru
+    $Install = Start-Process "MsiExec.exe" -ArgumentList $MSIArguments -NoNewWindow -ErrorAction Stop -ErrorVariable $IssueReported -PassThru
+    $Install | Wait-Process -Timeout 120 -ErrorAction SilentlyContinue -ErrorVariable $IssueReported
     
-    if ($Install.ExitCode -ne 0){
-        Write-Host "Install failed"
+    If ($IssueReported -or $Install.ExitCode){
+        Stop-Process -Name "msi*" -Force -ErrorAction SilentlyContinue
         Exit Try
     }
 }
 
 Catch{
     #Clean up both HKLM are of the uninstall portion of the registry
-    foreach($location in $uninstreglocations)
+    foreach($Location in $UninstRegLocations)
     {
-        foreach($application in $applicationlist)
+        foreach($Application in $ApplicationList)
         {
-            $results += (Get-ChildItem -Path $location | Get-ItemProperty | Where-Object {$_.DisplayName -like $application} | Select-Object -Property DisplayName,PSChildName)
+            $Results += (Get-ChildItem -Path $Location | Get-ItemProperty | Where-Object {$_.DisplayName -like $Application} | Select-Object -Property DisplayName,PSChildName)
         }
     }
     
-    foreach($UnInstRegRemoval in $results)
+    foreach($UnInstRegRemoval in $Results)
         {
-            $UninstCleanup = Join-path $uninstreglocations $UnInstRegRemoval.PSChildName
+            $UninstCleanup = Join-path $UninstRegLocations $UnInstRegRemoval.PSChildName
             Remove-Item $UninstCleanup -Recurse -Force -ErrorAction SilentlyContinue
         }
 
     #Clean up HKCR area of the installs
-    foreach($location in $classesreglocation)
+    foreach($Location in $ClassesRegLocation)
         {
-            foreach($application in $applicationlist)
+            foreach($Application in $ApplicationList)
             {
-                $results += (Get-ChildItem -Path $location | Get-ItemProperty | Where-Object {$_.ProductName -like $application} | Select-Object -Property ProductName,PSChildName)
+                $Results += (Get-ChildItem -Path $Location | Get-ItemProperty | Where-Object {$_.ProductName -like $Application} | Select-Object -Property ProductName,PSChildName)
             }
         }
     
     foreach($ClassesRegRemoval in $Results)
         {
-            $ClassesCleanup = Join-path $classesreglocation $ClassesRegRemoval.PSChildName
+            $ClassesCleanup = Join-path $ClassesRegLocation $ClassesRegRemoval.PSChildName
             Remove-Item $ClassesCleanup -Recurse -Force -ErrorAction SilentlyContinue
                     
         }
 
         #Try the install again
-        $Install = Start-Process "msiexec.exe" -ArgumentList "$MSIArguments"  -Wait -NoNewWindow -ErrorAction Stop -PassThru
+        $Install = Start-Process "MsiExec.exe" -ArgumentList "$MSIArguments"  -Wait -NoNewWindow -ErrorAction Stop -PassThru
 
-        Return $install.ExitCode
+        Return $Install.ExitCode
 }
 
       
