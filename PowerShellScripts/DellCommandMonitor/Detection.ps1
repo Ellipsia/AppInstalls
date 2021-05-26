0264e9f5ac869793cc2f73e8461d3971f9c2a7f0
Ellipsia <80127443+Ellipsia@users.noreply.github.com>
2021-05-26 13:26:58 -0700

    Add files via upload

---
+++ PowerShellScripts/DellCommandMonitor/Detection.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.Synopsis
+    Detects broken DCM installs
+.DESCRIPTION
+    Searches WMI to verify it is intact due to build upgrades or new features in DCIM.  If not, remediate.
+.EXAMPLE
+   
+#>
+
+#Variables to test WMI and DCIM
+$BIOSElementTest = Get-WmiObject -Namespace 'Root\DCIM\SYSMAN' -Class 'DCIM_BIOSElement' -ErrorAction SilentlyContinue
+$WarrantyInfoTest = Get-WmiObject -Namespace 'Root\DCIM\SYSMAN' -Class 'DCIM_AssetWarrantyInformation' -ErrorAction SilentlyContinue | Select Name
+
+# Check WMI to see if was a build upgrade and lost DCIM
+if ($BIOSElementTest -and ($WarrantyInfoTest.Name -ne 'Please set the value')){
+    Write-Output "Compliant"
+    Exit 0   
+}
+elseif (($BIOSElementTest -eq $null) -and ($WarrantyInfoTest -eq $null)){
+    Write-Output "Compliant"
+    Exit 0   
+}
+else{
+    Write-Output "Not Compliant"
+    Exit 1
+}
